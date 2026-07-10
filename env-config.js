0eeaa7a2e276ae259507ee8f10c9dc813ceb1956
Nandana <nandananair715@gmail.com>
2026-07-10 11:35:19 +0530

    Add environment configuration for Supabase

---
+++ env-config.js
@@ -0,0 +1,4 @@
+window.__ENV__ = {
+  SUPABASE_URL: "https://yitrbyifexlqwtlypjja.supabase.co/rest/v1/",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpdHJieWlmZXhscXd0bHlwamphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MjY0MDQsImV4cCI6MjA5OTIwMjQwNH0.c_8ctd-2FQQWur_4e32b-uvEmUSB2hgKW_OJ-1dMtLQ"
+};
